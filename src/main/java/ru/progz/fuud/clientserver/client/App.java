af0dabd864d20cbf490fb14c89b8e92ee0a05b2b
Bobin Fedor <bobin@reksoft.ru>
2010-06-05 17:47:58 +0400

    First Commit

---
+++ src/main/java/ru/progz/fuud/clientserver/client/App.java
@@ -0,0 +1,13 @@
+package ru.progz.fuud.clientserver.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
