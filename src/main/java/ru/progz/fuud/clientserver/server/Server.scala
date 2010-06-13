732bde67011f15df3815fbd3449130be75e615b1
Bobin Fedor <bobin@reksoft.ru>
2010-06-13 10:40:12 +0400

    Check is data invalid (non-latin characters)

--- src/main/java/ru/progz/fuud/clientserver/server/Server.scala
+++ src/main/java/ru/progz/fuud/clientserver/server/Server.scala
@@ -1,12 +1,15 @@
 package ru.progz.fuud.clientserver.server
 
-import java.io.{IOException, DataOutputStream, DataInputStream}
 import java.net.{Socket, ServerSocket}
 import ru.progz.fuud.clientserver.utils.Log4jLogger
 import ru.progz.fuud.clientserver.utils.Executor._
+import java.io._
 
 object Server extends Log4jLogger {
   val RESULT_OK = 0
+  val RESULT_FAIL = -1
+
+  val MAX_DATA_LENGTH = 10 * 1024 * 1024
 }
 
 class Server {
@@ -20,10 +23,59 @@ class Server {
     thread {
       count += 1
       logger.debug("about to process connection #" + count)
-      val inputStream = new DataInputStream(socket.getInputStream)
-      val data = inputStream.readUTF
-      new DataOutputStream(socket.getOutputStream).writeInt(RESULT_OK)
+
+      val data = readData(socket.getInputStream)
+
+      val outputStream: DataOutputStream = new DataOutputStream(socket.getOutputStream)
+
+      def writeResultAndCloseSocket(result: Int): Unit = {
+        outputStream.writeInt(result)
+        socket.close
+      }
+
+      data match {
+        case None => writeResultAndCloseSocket(RESULT_FAIL)
+        case Some(x) => {
+          writeResultAndCloseSocket(RESULT_OK)
+          saveValidData(new String(x))
+        }
+      }
     }
+  }
+
+  private def saveValidData(data: String) {
+    if (logger.isTraceEnabled) {
+      logger.trace("valid data to save: " + data)
+    }
+  }
+
+  private def readData(inputStream: InputStream) = {
+    val das = new DataInputStream(inputStream)
+    val length = das.readInt
+    if (length > MAX_DATA_LENGTH) {
+      None
+    } else {
+      // US-ASCII - 7-bit charset. Non-latin characters is not supported
+      val reader = new InputStreamReader(inputStream, "US-ASCII")
+      val result = new Array[Char](length)
+      try {
+        val wasRead = reader.read(result)
+
+        if (wasRead != length || !dataIsCorrect(result)) None else Some(result)
+
+      } catch {
+        case e: IOException => {
+          logger.debug("can not read data from client", e)
+          None
+        }
+      }
+    }
+  }
+
+  def dataIsCorrect(data: Array[Char]) = {
+    data.forall(c => {
+      (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')
+    })
   }
 
   def start(port: Int = 7777) {
--- src/test/java/ru/progz/fuud/clientserver/server/ServerTest.scala
+++ src/test/java/ru/progz/fuud/clientserver/server/ServerTest.scala
@@ -1,21 +1,19 @@
 package ru.progz.fuud.clientserver.server
 
 import org.scalatest.matchers.ShouldMatchers
-import org.scalatest.FlatSpec
 import org.scalatest.junit.JUnitSuite
-import javax.swing._
 import org.junit.Test
 import org.junit.Assert._
 import java.net.{Socket, ServerSocket}
-import java.io.{DataInputStream, DataOutputStream}
 import Server._
+import java.io.{OutputStreamWriter, ByteArrayOutputStream, DataInputStream, DataOutputStream}
 
-class ServerTest extends JUnitSuite with ShouldMatchers{
+class ServerTest extends JUnitSuite with ShouldMatchers {
   val port: Int = 8888
   val localhost = "127.0.0.1"
 
-  @Test
-  def startStop{
+  @Test(timeout = 10000)
+  def startStop {
     val server = new Server
 
     server.stop // close should not produce an exception if server is not started
@@ -31,22 +29,83 @@ class ServerTest extends JUnitSuite with ShouldMatchers{
     new ServerSocket(port).close // if port is free should be no exception here
   }
 
-  @Test(timeout=10000)
-  def send{
+  @Test(timeout = 10000)
+  def send {
     val server = new Server
     server.start(port)
 
+    val dataToSend = "HelloWorld"
+    val result = sendAndGetResult(dataToSend)
+
+    assertEquals(RESULT_OK, result)
+
+    server.stop
+  }
+
+  @Test/*(timeout = 10000)*/
+  def sendInvalidData {
+    withServer(server => {
+      /*"server" should "not accept non-latin characters" in*/ {
+
+        val illegalString = "Здравствуй, мир!!!"
+        val result = sendAndGetResult(illegalString)
+        assertEquals(RESULT_FAIL, result)
+      }
+    })
+  }
+
+
+  //--------------------------- Internal testes --------------------------
+
+  @Test
+  def test_StringToByteArray{
+    val str = "Hello World"
+    val result = StringToByteArray(str)
+
+    assertEquals(str.length, result.length)
+
+    for (i <- 0 until str.length){
+      assertEquals(str.charAt(i).toInt, result(i))
+    }
+  }
+
+
+  //--------------------------- Utils -------------------------------------
+
+  private def withServer(code: Server => Unit) {
+    val server = new Server
+    server.start(port)
+
+    code(server)
+
+    server.stop
+  }
+
+  private def sendAndGetResult(data: String): Int = {
+    sendAndGetResult(StringToByteArray(data))
+  }
+
+  private def sendAndGetResult(data: Array[Byte]): Int = {
     val clientSocketToSend = new Socket(localhost, port)
 
     val outputStream = new DataOutputStream(clientSocketToSend.getOutputStream)
     val inputStream = new DataInputStream(clientSocketToSend.getInputStream)
 
-    val dataToSend = "HelloWorld!!!"
-    outputStream.writeUTF(dataToSend)
+    outputStream.writeInt(data.length)
+    outputStream.write(data)
 
-    assertEquals(RESULT_OK, inputStream.readInt)
+    val result = inputStream.readInt
 
-    server.stop
+    clientSocketToSend.close
+
+    result
   }
 
+  private def StringToByteArray(s: String) = {
+    val bas = new ByteArrayOutputStream(s.length)
+    val osw = new OutputStreamWriter(bas, "KOI8-R") // to provide invalid data
+    osw.write(s)
+    osw.close
+    bas.toByteArray
+  }
 }
