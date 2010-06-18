7942ed8ac01f1bb416d91f4afe1632173397da76
Bobin Fedor <bobin@reksoft.ru>
2010-06-18 08:52:45 +0400

    Some refactoring.

--- src/main/java/ru/progz/fuud/clientserver/server/OnDiskDataStorage.scala
+++ src/main/java/ru/progz/fuud/clientserver/server/OnDiskDataStorage.scala
@@ -1,15 +1,14 @@
 package ru.progz.fuud.clientserver.server
 
 import java.lang.String
-import io.{Codec, Source}
-import java.nio.charset.Charset
+import io.Source
 import java.io.{File, FileOutputStream, OutputStreamWriter, BufferedWriter}
 
-class OnDiskDataStorage(file_name:String) extends DataStorage{
+class OnDiskDataStorage(file_name: String) extends DataStorage {
   val newLine = "\r\n"
   val os = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file_name), "US-ASCII"))
 
-  def put(data: String){
+  def put(data: String) {
     os.write(data)
     os.write(newLine)
   }
